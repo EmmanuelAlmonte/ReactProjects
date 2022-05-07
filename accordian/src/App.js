df3f8815492d5eaa5e99ee459538c26c18c1974c
EmmanuelAlmonte <emman9222@gmail.com>
2022-05-07 14:37:40 -0400

    Completed accordian react project

--- accordian/src/App.js
+++ accordian/src/App.js
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
-function App() {
-  return <h2>accordion project setup</h2>;
+import React, { useState } from 'react'
+import data from './data'
+import SingleQuestion from './Question'
+function App () {
+  const [questions, setQuestions] = useState(data)
+  const dataExtraction = data.map(({ id, title, info }) => {
+    return <SingleQuestion key={id} title={title} info={info} />
+  })
+  return (
+    <main>
+      <div className='container'>
+        <h3>questions and answers about login</h3>
+        <section>
+          {dataExtraction}
+        </section>
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
